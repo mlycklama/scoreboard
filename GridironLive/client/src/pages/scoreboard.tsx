e03d84ad8c34007a56db0f3026c5e02937895c68
mlycklama <mlycklama@idahostatesman.com>
2025-09-11 18:04:14 -0600

    Update scoreboard.tsx
    test to change breakpoint for columns

--- GridironLive/client/src/pages/scoreboard.tsx
+++ GridironLive/client/src/pages/scoreboard.tsx
@@ -117,7 +117,7 @@ export default function Scoreboard() {
 
       {/* Main Content */}
       <main className="max-w-7xl mx-auto p-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           {isLoading ? (
             // Loading skeletons
             Array.from({ length: 8 }).map((_, index) => (
