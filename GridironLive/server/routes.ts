53bea903429ec28df62f675e9e4c9a218fc1255f
mlycklama <mlycklama@idahostatesman.com>
2025-10-17 16:39:26 -0600

    Update URL for data fetching in routes.ts

--- GridironLive/server/routes.ts
+++ GridironLive/server/routes.ts
@@ -22,7 +22,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
       
       // Fetch fresh data
       const response = await fetch(
-        "https://script.googleusercontent.com/a/macros/idahostatesman.com/echo?user_content_key=AehSKLhk8VRYn3UM2Aon-TC7eFdqihB3NUO16t3GWFxreEUb-0bb8t7bYwT0svYvT1C2hhDR9sJ6H-D43ypcXFDhKVzs5jAJPcZLwGztb-nTlKdICW5ILY1WHDS1T3EO77SnL5m9XZQziwzCZcbuTeWzB2o2EaP7RA1oixeUG0pm9rWzy8kJrcs-ZT4NzGjaGu3235HVPTkd5CoZ1w-s7TtdVtSxDzFSm1wjkf9b74t2fcXYYZSDss3rspyPbqfsqmdBSPeNHORbjgejz0Z5_LzO5mLWl36U_gf5pdbgDv8cUeI9_XOoh_MM2Gt6siozxg&lib=MrNG-yONKM4TLUAB2VieHRrV78Ddeyst8"
+        "https://script.googleusercontent.com/a/macros/idahostatesman.com/echo?user_content_key=AehSKLgV5dbGiCQdIk9G63gz7MkOhtugoL19S1IBZd9IUh491Y_eD9_x0Td4BohdHalP6otcqwQF9FZZFdRwp89RDly3BbDuQUwgmNikwZalChvz7plqG0NMWzqfzYUe52c2_CBem2sEaFqJ08NbRIqGvi2lWuux9HesNn54S_gQeq9AlARk7hq9WOfx21zu-sq8Pxfwhg1gOGdzgrEDKhmBuXrwRiTK2AYj1_b_O_Edc3QP-IyLXg6gDVwZmVF6TkXkGQBvz0ugjrBsV6Dsa56ich7C_7WHEIe-uEBIgqv15pYSSZAJOObSte54I4P75g&lib=MrNG-yONKM4TLUAB2VieHRrV78Ddeyst8"
       );
       
       if (!response.ok) {
